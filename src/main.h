3feef933e55ceaa7cca7035dd17ad59fb1d20487
Entie1005 <tuannghialuudat@gmail.com>
2024-05-18 09:13:18 +0700

    volume

--- src/main.h
+++ src/main.h
@@ -5,3 +5,9 @@ void Redering(SDL_Texture* menuTexture, SDL_Texture* head2menu, SDL_Rect& headm2
 void RunGame(bool& quit, SDL_Texture* backgroundTexture, snake& SNAKE, SDL_Texture* appleTexture, SDL_Texture* gold, SDL_Texture* green, SDL_Event& e, snake2& SNAKE2, bool& win);
 
 void Winner(bool& result, SDL_Event& e, bool win, SDL_Texture* p1winTexture, Mix_Chunk* wingame, SDL_Texture* p2winTexture, SDL_Texture* space, SDL_Rect& sb);
+
+void increaseVolume();
+
+void decreaseVolume();
+
+void muteVolume();
